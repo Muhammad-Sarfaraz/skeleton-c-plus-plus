7e43085f0154907906ceed44bf96b81868e1741b
id.sent.me@gmail.com <id.sent.me@gmail.com>
2022-01-03 02:41:30 +0600

    initial commit

---
+++ skeleton.cpp
@@ -0,0 +1,26 @@
+/**
+ *    author:  Sarfaraz
+ *    created: 3.05.2019 17:51:34
+**/
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+void solve(){
+    int t;
+    cin >> t;
+      while (t--) {
+        cout<<"X";
+    }
+}
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+return 0;
+}
+
